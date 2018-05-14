259c03deac98d0c4217a96f6b78934eae5ad19fa
gedyra <gedyra@gmail.com>
2018-05-14 18:28:41 +0900

    4 parallelized

--- mandelbrotv2.c
+++ mandelbrotv2.c
@@ -39,6 +39,11 @@ struct Type_rgb{
   float b;
 };
 
+struct point {
+        int xstart, xend;   //描画する部分のX軸方向の始点，終点
+        int ystart, yend;   //描画する部分のY軸方向の始点，終点
+};
+
 /*
  * 画像中の各ピクセルの色を保持する配列
  */
@@ -50,23 +55,23 @@ struct Type_rgb pixels[IMAGE_Y * IMAGE_X];
  */
 struct Type_rgb setMyColor( int iteration )
 {
-  struct Type_rgb col;
-  if(iteration >= MAX_ITERATION){
-    /*
-     * 収束しなかったら黒
-     */
-    col.r =col.g = col.b = 0;
-  }else{
-    /*
-     * setting color pixel to the reset of the coordinate by the
-     * number of iteration before bailout.
-     */
+    struct Type_rgb col;
+    if(iteration >= MAX_ITERATION){
+        /*
+         * 収束しなかったら黒
+         */
+        col.r =col.g = col.b = 0;
+    }else{
+        /*
+         * setting color pixel to the reset of the coordinate by the
+         * number of iteration before bailout.
+         */
 #define PPSTEP 10
-    col.b = (iteration       % PPSTEP) / (float)PPSTEP ;
-    col.g = ((iteration /2 ) % PPSTEP) / (float)PPSTEP;
-    col.r = ((iteration /4 ) % PPSTEP) / (float)PPSTEP;
-  }
-  return col;
+        col.b = (iteration       % PPSTEP) / (float)PPSTEP ;
+        col.g = ((iteration /2 ) % PPSTEP) / (float)PPSTEP;
+        col.r = ((iteration /4 ) % PPSTEP) / (float)PPSTEP;
+    }
+    return col;
 }
 
 /*
@@ -76,32 +81,32 @@ struct Type_rgb setMyColor( int iteration )
  */
 int mandel_sub(float r0,float i0)
 {
-  /*
-   * r0 :- is the real part
-   * i0 :- is the imaginary
-   * r and i :- is the real and imaginary part of Zn.
-   * iteration :- is to keep control variable of the number of iteration
-   * MAX_ITERATION :- maximum number of iteration
-   *        (which is one of bailout condition)
-   */
+    /*
+     * r0 :- is the real part
+     * i0 :- is the imaginary
+     * r and i :- is the real and imaginary part of Zn.
+     * iteration :- is to keep control variable of the number of iteration
+     * MAX_ITERATION :- maximum number of iteration
+     *        (which is one of bailout condition)
+     */
 
-  float r, i, rtemp;
-  int iteration;
+    float r, i, rtemp;
+    int iteration;
 
-  r = 0;
-  i = 0;
-  iteration = 0;
-  /*
-   * (r*r) + (i*i) < (2*2) is the 2nd bailout condition
-   * i.e. the mandelbrot set is always within a radius of 2.
-   */
-  while(((r*r) + (i*i) < (2*2)) && iteration < MAX_ITERATION){
-    rtemp = (r*r) - (i*i) + r0;
-    i = (2*r*i) + i0;
-    r = rtemp;
-    iteration = iteration + 1;
-  }
-  return iteration;
+    r = 0;
+    i = 0;
+    iteration = 0;
+    /*
+     * (r*r) + (i*i) < (2*2) is the 2nd bailout condition
+     * i.e. the mandelbrot set is always within a radius of 2.
+     */
+    while(((r*r) + (i*i) < (2*2)) && iteration < MAX_ITERATION){
+        rtemp = (r*r) - (i*i) + r0;
+        i = (2*r*i) + i0;
+        r = rtemp;
+        iteration = iteration + 1;
+    }
+    return iteration;
 }
 
 /*
@@ -111,131 +116,145 @@ int mandel_sub(float r0,float i0)
  */
 void *mandelbrotset(void *datas)
 {
-  /*
-   * xx, yy はwindow上の座標。
-   * r0,i0 はそれを上記の複素平面にマップした座標。
-   */
-  float xx,yy,r0, i0;
-  int iteration;
-  long loc;
+    /*
+     * xx, yy はwindow上の座標。
+     * r0,i0 はそれを上記の複素平面にマップした座標。
+     */
+    float xx,yy,r0, i0;
+    int iteration;
+    long loc;
 
-  //画面上の座標に対して
-  for(yy = 0; yy < IMAGE_Y; yy++){
-    for(xx =0; xx < IMAGE_X; xx++){
+    struct point *p;
+    p = (struct point *)datas;
 
-      //複素平面上の座標に変換
-      i0= 2.1 * yy / IMAGE_Y -1.0;
-      r0= 3.6 * xx / IMAGE_X -2.5;
+    //画面上の座標に対して
+    for(yy = p->ystart; yy < p->yend; yy++){
+        for(xx = p->xstart; xx < p->xend; xx++){
 
-      //収束にかかる回数を計算
-      iteration=mandel_sub(r0,i0);
+            //複素平面上の座標に変換
+            i0= 2.1 * yy / IMAGE_Y -1.0;
+            r0= 3.6 * xx / IMAGE_X -2.5;
 
-      //画像を保持する配列に色情報を格納。
-      loc = yy*IMAGE_X + xx;
-      pixels[loc]= setMyColor(iteration);
+            //収束にかかる回数を計算
+            iteration=mandel_sub(r0,i0);
+
+            //画像を保持する配列に色情報を格納。
+            loc = yy*IMAGE_X + xx;
+            pixels[loc]= setMyColor(iteration);
+        }
     }
-  }
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
-
-
 
 /*
  * ----------------------------------------------------------------------------
  */
 void idle(void)
 {
-  glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void Init( )
 {
-  glViewport(0, 0, IMAGE_X, IMAGE_Y);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity( );
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity( );
-  gluOrtho2D(0, IMAGE_X, 0, IMAGE_Y);
+    glViewport(0, 0, IMAGE_X, IMAGE_Y);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity( );
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity( );
+    gluOrtho2D(0, IMAGE_X, 0, IMAGE_Y);
 
-  int i;
-  /*
-   * Initializing all the pixels to white.
-   */
-  for(i = 0; i < IMAGE_X * IMAGE_Y; i++){
-    pixels[i].r = 1;
-    pixels[i].g = 1;
-    pixels[i].b = 1;
-  }
+    int i;
+    /*
+     * Initializing all the pixels to white.
+     */
+    for(i = 0; i < IMAGE_X * IMAGE_Y; i++){
+        pixels[i].r = 1;
+        pixels[i].g = 1;
+        pixels[i].b = 1;
+    }
 }
 
 void onDisplay()
 {
-  /*
-   * pixels配列の中身を描画
-   */
-  glDrawPixels(IMAGE_X, IMAGE_Y, GL_RGB, GL_FLOAT, pixels);
-  glutSwapBuffers();
+    /*
+     * pixels配列の中身を描画
+     */
+    glDrawPixels(IMAGE_X, IMAGE_Y, GL_RGB, GL_FLOAT, pixels);
+    glutSwapBuffers();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-  switch (key) {
-    case 'q':
-    case 'Q':
-      glutIdleFunc(NULL);
-      exit(0);
-    default:
-      break;
-  }
+    switch (key) {
+        case 'q':
+        case 'Q':
+            glutIdleFunc(NULL);
+            exit(0);
+        default:
+            break;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-  pthread_t threads[4];
+    pthread_t threads[2][2];
 
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    struct point points[2][2];
 
-  glutInitWindowPosition (100, 100);
-  glutInitWindowSize (IMAGE_X, IMAGE_Y);
-  glutCreateWindow ("Mandelbrot");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-  Init();
+    glutInitWindowPosition (100, 100);
+    glutInitWindowSize (IMAGE_X, IMAGE_Y);
+    glutCreateWindow ("Mandelbrot");
 
-  glClearColor(1, 1, 1, 0);
-  glClear(GL_COLOR_BUFFER_BIT);
+    Init();
 
-  glutDisplayFunc(onDisplay);
-  glutKeyboardFunc(keyboard);
+    glClearColor(1, 1, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  glutIdleFunc(idle);
+    glutDisplayFunc(onDisplay);
+    glutKeyboardFunc(keyboard);
+
+    glutIdleFunc(idle);
 
 
-  // 別スレッドで mandelbrotset() を実行 
-  /*
-  if ( pthread_create( &mandelthread, NULL, 
-        mandelbrotset, NULL)){
+    int i, j;
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            points[i][j].xstart = j*IMAGE_X/2;
+            points[i][j].xend = points[i][j].xstart + IMAGE_X/2;
+            points[i][j].ystart = i*IMAGE_Y/2;
+            points[i][j].yend = points[i][j].ystart + IMAGE_Y/2;
+        }
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+        pthread_create(&threads[i][j], NULL, mandelbrotset, &points[i][j]);
+        //printf("thread%d started xstart = %d, 
+        //xend = %d, ystart = %d, yend = %d", i, points[i].xstart, points[i].xend, points[i].ystart, points[i].yend);
+    
+        }
+    }
+
+    /*
+    // 別スレッドで mandelbrotset() を実行 
+    if ( pthread_create( &mandelthread, NULL, 
+    mandelbrotset, NULL)){
     perror("mandelthread");
-  };
-  */
+    };
 
-  struct data {
-    int splitx;
-    int splity;
-  };
+*/
+    /* GLの描画ルーチンで計算中の配列を随時 描画する */
+    glutMainLoop();
 
-  int i;
-  for (i = 0; i < 1; i++) {
-    pthread_create(&threads[i], NULL, mandelbrotset, NULL);
-  }
+    for (i = 0; i < 2; i++) { 
+        for (j = 0; j < 2; j++) {
+            pthread_join(threads[i][j], NULL);
+        }
+    }
 
- // pthread_create(&th1, NULL, mandelbrotset, (void *)&d);
-
-  /* GLの描画ルーチンで計算中の配列を随時 描画する */
-  glutMainLoop();
-
-  pthread_join(threads[0], NULL);
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
